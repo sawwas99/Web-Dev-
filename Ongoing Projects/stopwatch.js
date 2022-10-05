f77b5afa6ca6f7aa4799b3505f81fa5a9837f1a4
songdavid00 <songdavid00@gmail.com>
2022-10-05 11:59:20 -0400

    Started stopwatch project

---
+++ Ongoing Projects/stopwatch.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+  
+    var seconds = 00; 
+    var tens = 00; 
+    var appendTens = document.getElementById("tens")
+  
+    buttonStart.onclick = function() {
+   
+    }
+    
+      buttonStop.onclick = function() {
+        
+    }
+    
+  
+    buttonReset.onclick = function() {
+       
+    }
+    
+     
+    
+    function startTimer () {
+      tens++; 
+    }
+    
+  
+  }
