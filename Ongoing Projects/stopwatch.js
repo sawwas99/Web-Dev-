9ccd2b02c9145f4e220992740b8c2cce41ae08ce
songdavid00 <songdavid00@gmail.com>
2022-10-06 21:04:06 -0400

    Added forms example from FreeCodeAcademy, and finished stopwatch

--- Ongoing Projects/stopwatch.js
+++ Ongoing Projects/stopwatch.js
@@ -1,27 +1,47 @@
 window.onload = function () {
-  
-    var seconds = 00; 
-    var tens = 00; 
+
+    var seconds = 00;
+    var tens = 00;
     var appendTens = document.getElementById("tens")
-  
-    buttonStart.onclick = function() {
-   
+    var appendSeconds = document.getElementById("seconds")
+    var buttonStart = document.getElementById('button-start');
+    var buttonStop = document.getElementById('button-stop');
+    var buttonReset = document.getElementById('button-reset');
+    var myInterval;
+
+    buttonStart.onclick = function () { //clearInterval built in function, clears timer
+        //myInterval uses setInterval, builtin func, to intialize a timer for every 10 seconds
+        clearInterval(myInterval); //https://www.w3schools.com/jsref/met_win_clearinterval.asp
+        myInterval = setInterval(startTimer, 10); //https://www.w3schools.com/jsref/met_win_setinterval.asp
     }
-    
-      buttonStop.onclick = function() {
-        
+
+    buttonStop.onclick = function () {
+        clearInterval(myInterval);
     }
-    
-  
-    buttonReset.onclick = function() {
-       
+
+    buttonReset.onclick = function () {
+        clearInterval(myInterval);
     }
-    
-     
-    
-    function startTimer () {
-      tens++; 
+
+    function startTimer() {
+        tens++;
+        if (tens <= 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+        if (tens > 9) {
+            appendTens.innerHTML = tens;
+        }
+        if (tens > 99) {
+            console.log("seconds");
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+        }
+        if (seconds > 9) {
+            appendSeconds.innerHTML = seconds;
+        }
     }
-    
-  
-  }
+
+
+}
